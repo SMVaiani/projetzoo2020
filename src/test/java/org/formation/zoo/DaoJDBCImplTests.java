71073ac4c4b1ecb4a31fbc16eae91d2a64bb999f
Paihere <Paihere@Paihere>
2020-03-12 18:47:28 +0100

    Test unitaire DaoJDBCImpl fait

--- src/test/java/org/formation/zoo/DaoJDBCImplTests.java
+++ src/test/java/org/formation/zoo/DaoJDBCImplTests.java
@@ -1,8 +1,12 @@
 package org.formation.zoo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
 
 import org.formation.zoo.service.CagePOJO;
+import org.formation.zoo.stockage.Dao;
 import org.formation.zoo.stockage.DaoFactory;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +14,7 @@ class DaoJDBCImplTests {
 
 	@Test
 	void testAjouter() {
-		CagePOJO obj;
+		CagePOJO obj = null;
 		obj = new CagePOJO();
 		obj.setCodeAnimal("Lion");
 		obj.setNom("Pai");
@@ -19,7 +23,71 @@ class DaoJDBCImplTests {
 		obj.setX(100);
 		obj.setY(350);
 		
-		DaoFactory.getInstance().getDao().ajouter(obj);
+		Dao<CagePOJO> cp = DaoFactory.getInstance().getDao();
+		cp.ajouter(obj);
+		List<CagePOJO> lcp = null;
+		lcp = cp.lireTous();
+		CagePOJO cagepojoLion = lcp.get(lcp.size()-1);
+		assertEquals("Lion", cagepojoLion.getCodeAnimal());
+		assertEquals("Pai", cagepojoLion.getNom());
+		assertEquals(12, cagepojoLion.getAge());
+		assertEquals(152.4, cagepojoLion.getPoids());
+		assertEquals(100, cagepojoLion.getX());
+		assertEquals(350, cagepojoLion.getY());
 	}
-
+	
+	@Test
+	void testModifier() {
+		CagePOJO obj = null;
+		Dao<CagePOJO> cp = DaoFactory.getInstance().getDao();
+		List<CagePOJO> lcp = null;
+		lcp = cp.lireTous();
+		obj = lcp.get(lcp.size()-1);
+		obj.setAge(15);
+		obj.setPoids(182.3);
+		cp.modifier(obj.getCle(), obj);
+		
+		assertEquals(15, obj.getAge());
+		assertEquals(182.3, obj.getPoids());
+	}
+	
+	@Test
+	void testEffacerCagePojoObj() {
+		CagePOJO obj = null;
+		Dao<CagePOJO> cp = DaoFactory.getInstance().getDao();
+		List<CagePOJO> lcp = null;
+		lcp = cp.lireTous();
+		int nbAnimalAvant = lcp.size();
+		obj = lcp.get(lcp.size()-1);
+		cp.effacer(obj);
+		
+		lcp = cp.lireTous();
+		
+		assertEquals(nbAnimalAvant-1, lcp.size());
+	}
+	
+	@Test
+	void testEffacerIntCle() {
+		testAjouter();
+		CagePOJO obj = null;
+		Dao<CagePOJO> cp = DaoFactory.getInstance().getDao();
+		List<CagePOJO> lcp = null;
+		lcp = cp.lireTous();
+		int nbAnimalAvant = lcp.size();
+		obj = lcp.get(lcp.size()-1);
+		cp.effacer(obj.getCle());
+		
+		lcp = cp.lireTous();
+		
+		assertEquals(nbAnimalAvant-1, lcp.size());
+	}
+	
+	@Test
+	void testLireTous() {
+		Dao<CagePOJO> cp = DaoFactory.getInstance().getDao();
+		List<CagePOJO> lcp = null;
+		lcp = cp.lireTous();
+		
+		assertNotNull(lcp);
+	}
 }
